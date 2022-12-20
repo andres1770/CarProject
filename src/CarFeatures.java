1954666e6856da7d3098fb258ad7c1999875667f
andres1770 <andresmoron1770@gmail.com>
2022-12-19 20:52:05 -0500

    the basic requirement is completed, launching the car

---
+++ src/CarFeatures.java
@@ -0,0 +1,7 @@
+public class CarFeatures {
+    int engine;
+    String make;
+    String color;
+    String doors;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+     CarFeatures XX= new CarFeatures();
+        System.out.println("launch car");
+    }
+}
