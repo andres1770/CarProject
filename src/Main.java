0489b034da6f73e41beef6014e072e75b003461b
andres1770 <andresmoron1770@gmail.com>
2022-12-19 21:15:07 -0500

    features of car have been added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
      CarFeatures XX= new CarFeatures();
         System.out.println("launch car");
+        XX.color="red";
+        XX.doors="5";
+        XX.engine=32323;
+        XX.make="a5";
+
     }
 }
